e8fb733de9dbbc94f19283dcec9e72667e5d9835
Kobalosz <imanuel.dartey@gmail.com>
2025-05-28 13:42:14 -0700

    Made currentDate, rightNow, currentMonth, currentDay, birthMonth, & birthDay variables. Began date check to see if currentMonth == birthMonth && currentDay == birthDay. added essential divs to index.html

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Rome-Core
 This is a fun little project website that I decided to do for someone really important to me.
+
+It is a simple website that'll display the current date and time, using that it will generate and produce a different inspirational qoute 
+or poem for the day!
--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+//* This is the current day down to the millisecond (Only used to make other variables)
+const currentDate = new Date()
+
+//* This is the current date down to the second (This will be displayed)
+const rightNow = currentDate.toLocaleString()
+
+//* These two variables contain the numeric value of the Month and Day, this will be used to perform the Checks
+const currentMonth = currentDate.getMonth()
+const currentDay = currentDate.getDate()
+
+//* These two variables contain the numeric values that point to July 24, the specific event date.
+const birthMonth = 6;
+const birthDay = 24;
 
 
-const currentDate = new Date().toLocaleString()
-const birthday = new Date('July 24, 2005')
+//* This if statement will be used to perform birthdate logic
+if(currentMonth == birthMonth && currentDay == birthDay){
+
+}
+
+console.log(rightNow);
 
 
-console.log(currentDate);
